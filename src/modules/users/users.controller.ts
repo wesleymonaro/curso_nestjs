fee8d5711d8ab1bbbc169e5389bdbc365afd5130
Wesley Monaro <wesleymonaro@hotmail.com>
2025-09-05 23:39:07 +0100

    Adicionados métodos de service e controller - Users

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,55 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common'
+import { UsersService } from './users.service'
 
-@Controller('users')
-export class UsersController {}
+@Controller({
+  version: '1',
+  path: 'users',
+})
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get()
+  findAll() {
+    return this.userService.findAll()
+  }
+
+  @Get(':userId')
+  async findOne(@Param('userId', ParseUUIDPipe) userId: string) {
+    const user = await this.userService.findById(userId)
+
+    if (!user) {
+      throw new NotFoundException('User not found')
+    }
+
+    return user
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() data: any) {
+    return this.userService.create(data)
+  }
+
+  @Put(':userId')
+  async update(@Param('userId', ParseUUIDPipe) userId: string, @Body() data: any) {
+    return this.userService.update(userId, data)
+  }
+
+  @Delete(':userId')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Param('userId', ParseUUIDPipe) userId: string) {
+    return this.userService.remove(userId)
+  }
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  findById(id: string) {
+    return this.prisma.user.findFirst({
+      where: {
+        id,
+      },
+    })
+  }
+
+  findByEmail(email: string) {
+    return this.prisma.user.findFirst({
+      where: {
+        email,
+      },
+    })
+  }
+
+  findAll() {
+    return this.prisma.user.findMany()
+  }
+
+  create(data: any) {
+    return this.prisma.user.create({
+      data,
+    })
+  }
+
+  update(id: string, data: any) {
+    return this.prisma.user.update({
+      where: {
+        id,
+      },
+      data,
+    })
+  }
+
+  async remove(id: string) {
+    await this.prisma.user.delete({
+      where: {
+        id,
+      },
+    })
+  }
+}
