127ef9be063f4f19fe6c196da1476343c28d6b76
Wesley Monaro <wesleymonaro@hotmail.com>
2025-11-08 13:58:46 +0000

    Criando os demais testes do ProjectsController

--- src/modules/projects/projects.controller.spec.ts
+++ src/modules/projects/projects.controller.spec.ts
@@ -44,4 +44,72 @@ describe('ProjectsController', () => {
       expect(service.findAll).toHaveBeenCalledTimes(1)
     })
   })
+
+  describe('findOne', () => {
+    it('should be able to return a single project by Id', async () => {
+      const project = mockedProjects[0]
+      const projectId = project.id
+      const expectedResult = {
+        ...project,
+        tasks: [],
+      }
+
+      jest.spyOn(service, 'findById').mockResolvedValue(expectedResult)
+      const response = await controller.findOne(projectId)
+
+      expect(response).toEqual(expectedResult)
+      expect(service.findById).toHaveBeenCalledWith(projectId)
+      expect(service.findById).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('create', () => {
+    it('should be able to create a new project', async () => {
+      const project = mockedProjects[0]
+
+      jest.spyOn(service, 'create').mockResolvedValue(project)
+
+      const response = await controller.create({
+        name: project.name,
+        description: project.description as string,
+      })
+
+      expect(response).toEqual(project)
+      expect(service.create).toHaveBeenCalledTimes(1)
+    })
+
+    it('should be able to handle validation errors', async () => {
+      const error = new Error('Name is required')
+
+      jest.spyOn(service, 'create').mockRejectedValue(error)
+
+      await expect(controller.create({ name: '', description: '' })).rejects.toThrow(
+        'Name is required',
+      )
+    })
+  })
+
+  describe('update', () => {
+    it('should be able to update a project', async () => {
+      const project = { ...mockedProjects[0], tasks: [] }
+
+      jest.spyOn(service, 'update').mockResolvedValue(project)
+
+      const response = await controller.update(project.id, {
+        name: project.name,
+        description: project.description as string,
+      })
+
+      expect(response).toEqual(project)
+      expect(service.update).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('remove', () => {
+    it('should be able to remove a project', async () => {
+      jest.spyOn(service, 'remove').mockImplementation()
+      await controller.remove(mockedProjects[0].id)
+      expect(service.remove).toHaveBeenCalledTimes(1)
+    })
+  })
 })
