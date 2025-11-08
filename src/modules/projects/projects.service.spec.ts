2798fc986ffed5b6557baa8e63bf051950ea8849
Wesley Monaro <wesleymonaro@hotmail.com>
2025-11-08 13:03:11 +0000

    Criando os demais testes do ProjectsService

--- src/modules/projects/projects.service.spec.ts
+++ src/modules/projects/projects.service.spec.ts
@@ -40,6 +40,16 @@ describe('ProjectsService', () => {
             project: {
               findMany: jest.fn(),
               count: jest.fn(),
+              findFirst: jest.fn(),
+              create: jest.fn(),
+              update: jest.fn(),
+              delete: jest.fn(),
+            },
+            projectCollaborator: {
+              create: jest.fn(),
+            },
+            task: {
+              deleteMany: jest.fn(),
             },
           },
         },
@@ -69,5 +79,51 @@ describe('ProjectsService', () => {
       paginateOutput<Project>(mockedProjects, mockedProjects.length, mockPaginationQuery),
     )
     expect(prisma.project.findMany).toHaveBeenCalledTimes(1)
+  })
+
+  it('should be able to return a project by Id', async () => {
+    const project = mockedProjects[0]
+
+    jest.spyOn(prisma.project, 'findFirst').mockResolvedValue(project)
+
+    const result = await service.findById(project.id)
+
+    expect(result).toEqual(project)
+    expect(prisma.project.findFirst).toHaveBeenCalledTimes(1)
+  })
+
+  it('should be able to create a new project', async () => {
+    const project = mockedProjects[0]
+    jest.spyOn(prisma.project, 'create').mockResolvedValue(project)
+
+    const result = await service.create({
+      name: project.name,
+      description: project.description as string,
+    })
+
+    expect(result).toEqual(project)
+    expect(prisma.project.create).toHaveBeenCalledTimes(1)
+  })
+
+  it('should be able to update a project', async () => {
+    const project = mockedProjects[0]
+    jest.spyOn(prisma.project, 'update').mockResolvedValue(project)
+
+    const result = await service.update(project.id, {
+      name: project.name,
+      description: project.description as string,
+    })
+
+    expect(result).toEqual(project)
+    expect(prisma.project.update).toHaveBeenCalledTimes(1)
+  })
+
+  it('should be able to remove a project', async () => {
+    const project = mockedProjects[0]
+
+    await service.remove(project.id)
+
+    expect(prisma.task.deleteMany).toHaveBeenCalledTimes(1)
+    expect(prisma.project.delete).toHaveBeenCalledTimes(1)
   })
 })
